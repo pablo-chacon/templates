c5587bb1fd0e6c368db5a22e82782f702b37ae0a
pablo-chacon <Snemilio@protonmail.com>
2023-09-01 11:18:44 +0200

    Rest template Spring Boot

---
+++ src/main/java/com/example/springbackendtemplate/SpringbackendtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbackendtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbackendtemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbackendtemplateApplication.class, args);
+    }
+
+}
